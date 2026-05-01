7d6a0777c342bb79c34c49ebb0057b1bc992e4e4
person man <busseaters@gmail.com>
2026-04-30 18:19:20 -0700

    Add hamcraftmain.js for HamCraft UB games page

---
+++ Others/hamcraftmain.js
@@ -0,0 +1,244 @@
+(function(){
+if(!document.head) document.documentElement.appendChild(document.createElement("head"));
+(function(parent){
+const el_27 = document.createElement("meta");
+el_27.setAttribute("charset", `UTF-8`);
+parent.appendChild(el_27);
+const el_28 = document.createElement("meta");
+el_28.setAttribute("name", `viewport`);
+el_28.setAttribute("content", `width=device-width, initial-scale=1.0`);
+parent.appendChild(el_28);
+const el_29 = document.createElement("title");
+(function(parent){
+parent.appendChild(document.createTextNode(`Games Page For HamCraft UB`));
+})(el_29);
+parent.appendChild(el_29);
+const el_30 = document.createElement("link");
+el_30.setAttribute("rel", `stylesheet`);
+el_30.setAttribute("href", `https://cdn.jsdelivr.net/gh/HamCraft-UB/Singlefile@main/assets/games.css`);
+parent.appendChild(el_30);
+const el_31 = document.createElement("link");
+el_31.setAttribute("rel", `icon`);
+el_31.setAttribute("type", `image/png`);
+el_31.setAttribute("href", `https://cdn.jsdelivr.net/gh/HamCraft-UB/Singlefile@main/assets/hamcraft-ub-icon.png`);
+parent.appendChild(el_31);
+})(document.head);
+if(!document.body) document.documentElement.appendChild(document.createElement("body"));
+(function(parent){
+const el_32 = document.createElement("div");
+el_32.setAttribute("class", `top`);
+(function(parent){
+})(el_32);
+(function(parent){
+const el_33 = document.createElement("h1");
+(function(parent){
+parent.appendChild(document.createTextNode(`HamCraft UB`));
+})(el_33);
+parent.appendChild(el_33);
+})(el_32);
+(function(parent){
+})(el_32);
+(function(parent){
+const el_34 = document.createElement("p");
+(function(parent){
+parent.appendChild(document.createTextNode(`Games`));
+})(el_34);
+parent.appendChild(el_34);
+})(el_32);
+(function(parent){
+})(el_32);
+parent.appendChild(el_32);
+const el_35 = document.createElement("div");
+(function(parent){
+})(el_35);
+(function(parent){
+const el_36 = document.createElement("a");
+el_36.setAttribute("target", `_blank`);
+el_36.setAttribute("class", `bloxy-rainbow-btn bloxy-btn`);
+el_36.setAttribute("href", `https://bloxcraft-ubg.pages.dev?utm_source=hamcraft-ub.github.io`);
+(function(parent){
+parent.appendChild(document.createTextNode(`More Games`));
+})(el_36);
+parent.appendChild(el_36);
+})(el_35);
+(function(parent){
+})(el_35);
+parent.appendChild(el_35);
+const el_37 = document.createElement("div");
+el_37.setAttribute("id", `controls`);
+(function(parent){
+})(el_37);
+(function(parent){
+const el_38 = document.createElement("input");
+el_38.setAttribute("id", `searchBox`);
+el_38.setAttribute("placeholder", `Search games...`);
+parent.appendChild(el_38);
+})(el_37);
+(function(parent){
+})(el_37);
+parent.appendChild(el_37);
+const el_39 = document.createElement("div");
+(function(parent){
+})(el_39);
+(function(parent){
+const el_40 = document.createElement("p");
+el_40.setAttribute("id", `gameCount`);
+(function(parent){
+parent.appendChild(document.createTextNode(`Games: 0`));
+})(el_40);
+parent.appendChild(el_40);
+})(el_39);
+(function(parent){
+})(el_39);
+parent.appendChild(el_39);
+const el_41 = document.createElement("div");
+el_41.setAttribute("id", `views`);
+(function(parent){
+})(el_41);
+(function(parent){
+const el_42 = document.createElement("p");
+(function(parent){
+parent.appendChild(document.createTextNode(`Scroll down to see all games`));
+})(el_42);
+parent.appendChild(el_42);
+})(el_41);
+(function(parent){
+})(el_41);
+parent.appendChild(el_41);
+const el_43 = document.createElement("div");
+el_43.setAttribute("class", `wrap`);
+(function(parent){
+})(el_43);
+(function(parent){
+const el_44 = document.createElement("div");
+el_44.setAttribute("id", `gameGrid`);
+el_44.setAttribute("class", `grid`);
+parent.appendChild(el_44);
+})(el_43);
+(function(parent){
+})(el_43);
+(function(parent){
+const el_45 = document.createElement("div");
+el_45.setAttribute("id", `status`);
+el_45.setAttribute("style", `display:none; padding:24px;`);
+(function(parent){
+parent.appendChild(document.createTextNode(`Loading...`));
+})(el_45);
+parent.appendChild(el_45);
+})(el_43);
+(function(parent){
+})(el_43);
+parent.appendChild(el_43);
+const el_46 = document.createElement("div");
+el_46.setAttribute("id", `viewerOverlay`);
+(function(parent){
+})(el_46);
+(function(parent){
+const el_47 = document.createElement("div");
+el_47.setAttribute("id", `viewerWindow`);
+(function(parent){
+})(el_47);
+(function(parent){
+const el_48 = document.createElement("div");
+el_48.setAttribute("id", `viewerHeader`);
+(function(parent){
+})(el_48);
+(function(parent){
+const el_49 = document.createElement("span");
+el_49.setAttribute("id", `viewerTitle`);
+(function(parent){
+parent.appendChild(document.createTextNode(`Loading...`));
+})(el_49);
+parent.appendChild(el_49);
+})(el_48);
+(function(parent){
+})(el_48);
+(function(parent){
+const el_50 = document.createElement("div");
+(function(parent){
+})(el_50);
+(function(parent){
+const el_51 = document.createElement("button");
+el_51.setAttribute("id", `newTabBtn`);
+(function(parent){
+parent.appendChild(document.createTextNode(`New Tab`));
+})(el_51);
+parent.appendChild(el_51);
+})(el_50);
+(function(parent){
+})(el_50);
+(function(parent){
+const el_52 = document.createElement("button");
+el_52.setAttribute("id", `closeBtn`);
+(function(parent){
+parent.appendChild(document.createTextNode(`Close`));
+})(el_52);
+parent.appendChild(el_52);
+})(el_50);
+(function(parent){
+})(el_50);
+parent.appendChild(el_50);
+})(el_48);
+(function(parent){
+})(el_48);
+parent.appendChild(el_48);
+})(el_47);
+(function(parent){
+})(el_47);
+(function(parent){
+const el_53 = document.createElement("iframe");
+el_53.setAttribute("id", `viewerFrame`);
+el_53.setAttribute("sandbox", `allow-scripts allow-same-origin allow-forms`);
+parent.appendChild(el_53);
+})(el_47);
+(function(parent){
+})(el_47);
+parent.appendChild(el_47);
+})(el_46);
+(function(parent){
+})(el_46);
+parent.appendChild(el_46);
+})(document.body);
+
+const externalScripts = [
+  {
+    "type": "external",
+    "src": "https://cdn.jsdelivr.net/gh/nobody81754/modded-htmls@latest/Others/hamcraft.js"
+  }
+];
+
+
+function loadScriptsSequentially(list, done){
+    if(list.length === 0){ done(); return; }
+
+    const item = list.shift();
+    const s = document.createElement("script");
+
+    if(item.type === "external"){
+        s.src = item.src;
+    } else if(item.type === "blob"){
+        s.src = URL.createObjectURL(new Blob([item.code], {type:"text/javascript"}));
+    }
+
+    s.onload = () => loadScriptsSequentially(list, done);
+    s.onerror = () => { console.error("SCRIPT FAILED:", item); loadScriptsSequentially(list, done); };
+
+    document.head.appendChild(s);
+}
+
+const inlineScripts = [];
+
+function runInlineScripts(){
+    inlineScripts.forEach(code => {
+        const s = document.createElement("script");
+        s.textContent = code;
+        document.body.appendChild(s);
+    });
+
+    if(typeof onAllScriptsLoaded === "function"){
+        onAllScriptsLoaded();
+    }
+}
+
+loadScriptsSequentially([...externalScripts], runInlineScripts);
+})();
